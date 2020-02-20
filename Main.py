56ad8f785e8d547262980553ea75b4e0307592e0
wesley-debrusk <wesdebrusk@gmail.com>
2020-02-19 20:44:48 -0500

    database stuff

---
+++ Main.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+
+config = {
+'user': 'root',
+'password': 'databasepass',
+'host': '127.0.0.1',
+'database': 'timesheet',
+'raise_on_warnings': True
+}
+
+try:
+	cnx = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Something is wrong with your user name or password")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist")
+	else:
+		print(err)
+else:
+	print("success")
+	cnx.close()
